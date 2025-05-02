e3b5f3a5780a3ff7169466f21d77e946f2553a08
dina <dina2022001@gmail.com>
2025-05-02 15:51:22 +0300

    promise sec

--- Promise/index.js
+++ Promise/index.js
@@ -1,9 +1,34 @@
 const { Observable } = require('rxjs');
-const promise=new Promise(callback);
-const observable=new Observable(callback).subscribe();
-function callback(){
+const promise = new Promise((res, rej) => {
+    res()
+}).then((data) => {
+    console.log("Promise then:", data);
+});
+const observable = new Observable(callback).subscribe();
+function callback() {
     console.log("angular");
-    
 }
 console.log(promise);
 console.log(observable);
+
+// const { Observable } = require('rxjs');
+
+// // Promise
+// const promise = new Promise((resolve, reject) => {
+//   console.log("Promise is running...");
+//   resolve("Promise resolved!");
+// }).then((data) => {
+//   console.log("Promise then:", data);
+// });
+
+// // Observable
+// const observable = new Observable((subscriber) => {
+//   console.log("Observable is running...");
+//   subscriber.next("Observable emitted!");
+//   subscriber.complete();
+// });
+
+// observable.subscribe({
+//   next: (value) => console.log("Observable next:", value),
+//   complete: () => console.log("Observable complete")
+// });
